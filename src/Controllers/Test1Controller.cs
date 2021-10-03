14e432f07c050fea5f590b3c99d4f2f0d7cf34a4
JasonYen62 <yen6488@hotmail.com>
2021-10-04 03:07:34 +0800

    Init project

---
+++ src/Controllers/Test1Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using jasonwebtest.Models;
+
+namespace jasonwebtest.Controllers
+{//data : https://data.gov.tw/dataset/29961
+    public class Test1Controller : Controller
+    {
+        private readonly ILogger<Test1Controller> _logger;
+
+        public Test1Controller(ILogger<Test1Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
